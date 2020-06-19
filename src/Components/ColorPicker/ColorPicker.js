73cf4636d049a01c73b18a0cab3750d8e49e809a
unknown <luketpena@gmail.com>
2020-06-18 23:13:25 -0500

    color picker applies color to box

--- src/Components/ColorPicker/ColorPicker.js
+++ src/Components/ColorPicker/ColorPicker.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import iro from '@jaames/iro';
 
 import blend from '../../Modules/blend';
 
@@ -7,16 +8,32 @@ const Container = styled.div`
 
 `;
 
-const ColorBox = styled.div`
+const ColorBox = styled.div.attrs(props=>({
+    style: {
+        backgroundColor: props.color,
+    },
+}))`    
     width: 250px;
-    height: 100px;
-    background-color: ${props=>props.color};
-`;
+    height: 100px;`;
+    
+
 
 export default function ColorPicker() {
+
+    const [myCol, setMyCol] = useState('#000000');
+
+    useEffect(()=>{
+        var colorPicker = new iro.ColorPicker('#picker');
+        colorPicker.on('color:change', function(color) {
+            setMyCol(color.hexString)
+        })
+    },[])
+   
+
     return (
         <Container>
-            <ColorBox color={blend('#FF0000','#0000FF',1)} />
+            <ColorBox color={blend('#FF0000',myCol,1)} />
+            <div id="picker"></div>
         </Container>
     )
 }
