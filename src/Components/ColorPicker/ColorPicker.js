1bb8584266d064761e26fe129741ec77ca871bad
unknown <luketpena@gmail.com>
2020-06-19 00:06:13 -0500

    Can select cell and change color using picker

--- src/Components/ColorPicker/ColorPicker.js
+++ src/Components/ColorPicker/ColorPicker.js
@@ -12,27 +12,60 @@ const ColorBox = styled.div.attrs(props=>({
     style: {
         backgroundColor: props.color,
     },
-}))`    
-    width: 250px;
+}))`width: 250px;
     height: 100px;`;
     
-
+const ColorButton = styled.button.attrs(props=>({
+    style: {
+        backgroundColor: props.color,
+        border: (props.selected? '1px solid red' : '1px solid rgba(0,0,0,0)')
+    },
+}))`
+    height: 24px;
+    width: 24px;
+    outline: none;
+    border-radius: 50%;
+`;
 
 export default function ColorPicker() {
 
-    const [myCol, setMyCol] = useState('#000000');
+    const [selectedColor, setSelectedColor] = useState('#000000');
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const [colorList, setColorList] = useState(['#FFF']);
 
     useEffect(()=>{
         var colorPicker = new iro.ColorPicker('#picker');
-        colorPicker.on('color:change', function(color) {
-            setMyCol(color.hexString)
-        })
-    },[])
+        colorPicker.on('color:change', color=> {
+            setSelectedColor(color.hexString);
+        });
+    },[]);
+
+    
    
+    function renderColorList() {
+        return colorList.map((color,i)=>{
+            return <ColorButton key={i} color={(i===selectedIndex? selectedColor : color)} selected={i===selectedIndex} onClick={()=>selectColor(i,color)}/>
+        })
+    }
+
+    function selectColor(i,color) {
+        if (selectedIndex!==i) {
+            setSelectedIndex(i);
+            setSelectedColor(color);
+        } else {
+            setSelectedIndex(-1);
+            var colorListCopy = [...colorList];
+            colorListCopy[i] = selectedColor;
+            setColorList(colorListCopy);
+        }
+    }
 
     return (
         <Container>
-            <ColorBox color={blend('#FF0000',myCol,1)} />
+            {renderColorList()}
+            {selectedIndex}
+            <button>+</button>
+            <ColorBox color={blend('#FF0000',colorList[0],1)} />
             <div id="picker"></div>
         </Container>
     )
