852dc9d3c624f9f56fcd7afea75af3a31a4df4b7
Luke Peña <luketpena@gmail.com>
2020-07-27 18:28:44 -0500

    Clean and comment

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux';
 
-/* import reducers here */
 import progressReducer from './progress.reducer';
 import channelsReducer from './channels.reducer';
 
 const rootReducer = combineReducers({
-    //List reducers separated by comma
     progressReducer,
     channelsReducer
 });
--- src/Redux/progress.reducer.js
+++ src/Redux/progress.reducer.js
@@ -1,3 +1,5 @@
+//This reducer simply tracks the progress of the animation bar
+
 const reducer = (state=.5, action)=>{
     switch(action.type) {
         case 'SET_PROGRESS': return action.payload;
